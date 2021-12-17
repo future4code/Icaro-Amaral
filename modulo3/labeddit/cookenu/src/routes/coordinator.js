abdf2c9c1207f4cf4a1a128883339205052cab5c
IcaroFA <icaroadvocacia@gmail.com>
2021-12-16 23:25:11 -0300

    atualizado 16/12

---
+++ modulo3/labeddit/cookenu/src/routes/coordinator.js
@@ -0,0 +1,19 @@
+export const goToLogin = (history) => {
+    history.push("/login")
+}
+
+export const goToLogin = (history) => {
+    history.push("/cadastro")
+}
+
+export const goToLogin = (history) => {
+    history.push("/adicionar-receita")
+}
+
+export const goToLogin = (history) => {
+    history.push(`/detalhe/${id}`)
+}
+
+export const goToLogin = (history) => {
+    history.push("/")
+}
