1c285c15f37689c05a219108fdd253cdf8081b48
IcaroFA <icaroadvocacia@gmail.com>
2022-01-27 23:31:22 -0300

    arquivo data correto

--- modulo4/api-rest/api-rest/src/data.ts
+++ modulo4/api-rest/api-rest/src/data.ts
@@ -14,16 +14,44 @@ type UserTypes = {
   export let users: UserTypes[] = [
     {
       id: 1,
-      name: 'Evelim',
-      email: 'evelimmg@email.com',
+      name: 'Alice',
+      email: 'alice@email.com',
       type: TYPE.ADMIN,
-      age: 28,
+      age: 12,
     },
     {
       id: 2,
-      name: 'Vlad',
-      email: 'vladslash1987@email.com',
+      name: 'Bob',
+      email: 'bob@email.com',
       type: TYPE.NORMAL,
-      age: 18,
+      age: 36,
     },
-];
+    {
+      id: 3,
+      name: 'Coragem',
+      email: 'coragem@email.com',
+      type: TYPE.NORMAL,
+      age: 21,
+    },
+    {
+      id: 4,
+      name: 'Dory',
+      email: 'dory@email.com',
+      type: TYPE.NORMAL,
+      age: 17,
+    },
+    {
+      id: 5,
+      name: 'Elsa',
+      email: 'elsa@email.com',
+      type: TYPE.ADMIN,
+      age: 17,
+    },
+    {
+      id: 6,
+      name: 'Fred',
+      email: 'fred@email.com',
+      type: TYPE.ADMIN,
+      age: 60,
+    },
+  ];
